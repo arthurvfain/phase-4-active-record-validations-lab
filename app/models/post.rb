e06d0a4b90943cc991cbc0ede80711007db0df39
Arthur V Fain <arthurvfain@gmail.com>
2021-07-22 16:04:06 -0400

    commit tree 38a0c3ada6e1072411017cb21a3c8198d216352c

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence:true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    # validates :category, matches: { 'Fiction' || 'Non-Fiction' }
+    validate :fic_nonfic
+
+    def fic_nonfic
+        unless category.match?(/Fiction/||/Non-Fiction/)
+            errors.add(:category, "Only Fiction or Non-Fiction please")
+        end
+    end
 end
